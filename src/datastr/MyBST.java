0f4c93fee5bbdfa38b46d52bee7a45f6d94bd667
NaurisJ <nauris.javalds@gmail.com>
2025-03-20 10:31:31 +0200

    ADD: added add function in MyBST

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -26,4 +26,55 @@ public class MyBST <Ttype>{
 		}
 	}
 	
+	
+	public void add(Ttype element) throws Exception{
+		if (element == null) {
+			throw new Exception("Element can not be null!");
+		}
+		
+		//TODO ja ir pirmais elements, tad izveidot jaunu mezglu un to uzlikt ka saknes mezglu
+		
+		if (!isFull()) {
+			if (isEmpty()) {
+				MyBSTNode<Ttype> newNode = new MyBSTNode(element);
+				root = newNode;
+				counter++;
+			} else {
+				addHelper(element,root);
+			}
+		} else {
+			throw new Exception("BST is full!");
+		}
+	}
+	
+	private void addHelper(Ttype element, MyBSTNode<Ttype> currentNode) {
+		//parbaudam pa kuru pusi elements tiks pievienots.
+		
+		// ja elements ir lielaks par currentNode elementu, tad japarvietojas uz labo pusi.
+		if (((Comparable)element).compareTo(currentNode.getElement()) == 1) {
+			// ja neeksiste labais bersn ,tad var veidot jaunu mezglu un pielipinat kaa labo bernu
+			if (currentNode.getRightCh() == null) {
+				MyBSTNode<Ttype> newNode = new MyBSTNode<>(element);
+				currentNode.setRightCh(newNode);
+				newNode.setParent(currentNode);
+				counter++;
+			} else {
+				addHelper(element, currentNode.getRightCh());
+			}
+			
+			
+		}else if (((Comparable)element).compareTo(currentNode.getElement()) == -1) {
+			if (currentNode.getLeftCh() == null) {
+				MyBSTNode<Ttype> newNode = new MyBSTNode<>(element);
+				currentNode.setLeftCh(newNode);
+				newNode.setParent(currentNode);
+				counter++;
+			} else {
+				addHelper(element,currentNode.getLeftCh());
+			}
+		}
+		
+		// ja elements ir mazakaks par currentNode elementu, tad japarvirzas uz kreiso pusi
+	}
+	
 }
